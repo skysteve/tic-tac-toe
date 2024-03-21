c299fd22fdc7f85d303b132c19d697ab797e3d47
Steve Jenkins <github@skysteve.com>
2024-03-21 09:16:02 -0700

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,33 @@ export function App() {
 
         <br />
         <Board />
+
+        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+          <div className="col-md-4 d-flex align-items-center">
+            <span className="mb-3 mb-md-0 text-body-secondary">
+              © 2024 Steve Jenkins
+            </span>
+          </div>
+
+          <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+            <li className="ms-3">
+              <a
+                href="https://www.skysteve.com/"
+                className="nav-link px-2 text-body-secondary"
+              >
+                Home
+              </a>
+            </li>
+            <li className="ms-3">
+              <a
+                href="https://github.com/skysteve/tic-tac-toe"
+                className="nav-link px-2 text-body-secondary"
+              >
+                Source Code
+              </a>
+            </li>
+          </ul>
+        </footer>
       </div>
     </GameContext.Provider>
   );
